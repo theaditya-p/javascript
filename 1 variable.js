2ed2072940b0879c252663541c9b95be504d6903
Aditya Babasaheb Patil <adityapatil0016@gmail.com>
2026-02-25 00:28:33 +0530

    initial commit

---
+++ 1 variable.js
@@ -0,0 +1,17 @@
+const accountId = 144455
+let accountEmail = "aditya0037@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "jdv.com"
+accountPassword = "746765"
+accountCity = "pune"
+
+console.log("accountId");
+
+/* prefer not to use var */
+
+console.table([accountId, accountEmail, accountPassword, accountCity ])
+
---
+++ 2 datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; //treat all JS code as a new version//
+
+//alert(3 + 3) //we are using nodejs note browser//
+
+console.log(3 + 3)
