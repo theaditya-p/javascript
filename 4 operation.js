57c6d29f4323121be1d647af62d597fdffad957e
Aditya Babasaheb Patil <adityapatil0016@gmail.com>
2026-03-02 01:38:25 +0530

    basics

---
+++ 4 operation.js
@@ -0,0 +1,22 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello"
+let str2 = "world"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2); // result is 12 because 2 is converted to string and concatenated with "1"
+//comsole.log(1 + "2"); // result is 12 because 1 is converted to string and concatenated with "2"
+
+console.log(+true) //+true is converted to 1
+console.log(+"")  //empty string is converted to 0
+
+
+/*let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1, num2, num3);*/
+
+
---
+++ 5 comparison.js
@@ -0,0 +1,6 @@
+//console.log(2 > 1); // true
+//console.log(2 == 1); // false
+//console.log(2 === 1); // false
+
+//console.log("2" > 1); // true, because "2" is converted to 2
+console.log(null >= 0); //
